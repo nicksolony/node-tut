a6e0497de2191912edca691e10fe6787198fda87
nicksolony <nicksolony@gmail.com>
2023-07-23 13:00:15 -0400

    tested import export

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const message = "NodeJS in amazing!";
-console.log(message);
+import { hello, goodbye } from "./greeter.js";
 
-const validator = require("validator");
+// const message = "NodeJS in amazing!";
+// console.log(message);
 
-const validateEmail = email => {
-    return validator.isEmail(email);
-};
+// const validator = require("validator");
 
-console.log(
-    "Is mango@mail.com a valid email?: ",
-    validateEmail("mango@mail.com")
-);
+// const validateEmail = email => {
+//     return validator.isEmail(email);
+// };
 
-console.log(
-    "Is Mangozedog.com a valid email?: ",
-    validateEmail("Mangozedog.com")
-  );
+// console.log(
+//     "Is mango@mail.com a valid email?: ",
+//     validateEmail("mango@mail.com")
+// );
+
+// console.log(
+//     "Is Mangozedog.com a valid email?: ",
+//     validateEmail("Mangozedog.com")
+//   );
+
+
+
+console.log(hello()); // "hello!"
+console.log(goodbye()); // "goodbye!"
